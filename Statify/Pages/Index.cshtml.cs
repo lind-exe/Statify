fcd35050880bc7f15f46ad3567c4da37865a1518
Jonathan Lind <jojje_lind@live.se>
2023-11-21 15:59:09 +0100

    removed loggers

--- Statify/Pages/Index.cshtml.cs
+++ Statify/Pages/Index.cshtml.cs
@@ -5,13 +5,6 @@ namespace Statify.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
-
         public void OnGet()
         {
             
